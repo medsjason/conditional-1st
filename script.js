6ba4e65ab367ba3070a8f0547bbaed66f6c4ee1d
medsjason <108416074+medsjason@users.noreply.github.com>
2022-08-09 01:53:16 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ cond_form.addEventListener("submit", function(e){
     e.preventDefault();
 
     if(user_input.value >= 70 && user_input.value <= 100){
-        document.write("A excellent")
+        result.innerHTML ="A excellent"
     }else
     if(user_input.value >= 60 && user_input.value <= 69){
-        document.write("B very good")
+        result.innerHTML ="B very good"
     }else
     if(user_input.value >= 50 && user_input.value <= 59){
-        document.write("C Average")
+        result.innerHTML ="C average"
     }else
     if(user_input.value >= 40 && user_input.value <= 49){
-        document.write("D pass")
+        result.innerHTML ="D pass"
     }else
     if(user_input.value >= 0 && user_input.value <= 39){
-        document.write("F fail")
+        result.innerHTML ="F fail"
     }
 })
